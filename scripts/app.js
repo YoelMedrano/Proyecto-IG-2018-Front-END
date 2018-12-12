4d1aa325a2f30b896c4a06edc96caa5a0bb4dc84
YoelMedrano <yoelmolina2007@gmail.com>
2018-12-12 17:45:23 -0400

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -53,7 +53,7 @@ angular.module('angularRestfulAuth', [
  
         $rootScope.$on('$locationChangeStart', function (event, next, current) {
             // redirect to login page if not logged in
-            if ($location.path() !== '/login' || $location.path() !== '/registro' || $location.path() !== '/solicitud'  && !$rootScope.globals.currentUser ) {
+            if ($location.path() !== '/login' && $location.path() !== '/registro' && $location.path() !== '/solicitud'  && !$rootScope.globals.currentUser ) {
                 window.location="#/home";
                 
             }
