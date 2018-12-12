866cca036724322cd87738c4e9a326a1d3f38c71
YoelMedrano <yoelmolina2007@gmail.com>
2018-12-12 18:29:43 -0400

    Update controller.js

--- services/login/controller.js
+++ services/login/controller.js
@@ -6,7 +6,7 @@ angular.module('Authentication')
     ['$scope', '$rootScope', '$location', 'AuthenticationService',
     function ($scope, $rootScope, $location, AuthenticationService) {
         // reset login status
-        
+        AuthenticationService.ClearCredentials();
  
         $scope.login = function () {
             $scope.dataLoading = true;
