42c6bd3d325bb27a9073ac0c07d4393380f5f4b3
YoelMedrano <yoelmolina2007@gmail.com>
2018-12-13 17:11:23 -0400

    Update services.js

--- services/login/services.js
+++ services/login/services.js
@@ -11,8 +11,8 @@ angular.module('Authentication')
             
              $http.post( 'https://proyecto-paquetes-ings.herokuapp.com/cliente/login', {correoElectronico : correoElectronico , contrasena : contrasena})
               .success(function (response,data){
-                       var response= { success: correoElectronico && contrasena };
-                       $rootScope.success=data;
+                       var response= { success: correoElectronico && contrasena && idCliente };
+
                        callback(response);
                       
             },1000)
