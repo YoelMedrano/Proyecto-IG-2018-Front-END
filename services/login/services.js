e450ee3d6c7d81f8a9b76e28c44bd122984aadbb
YoelMedrano <yoelmolina2007@gmail.com>
2018-12-12 23:28:45 -0400

    Update services.js

--- services/login/services.js
+++ services/login/services.js
@@ -41,7 +41,7 @@ angular.module('Authentication')
         };
  
         service.SetCredentials = function (correoElectronico, contrasena) {
-            var authdata = "123";
+            var authdata = $rootScope.idCliente;
  
             $rootScope.globals = {
                 currentUser: {
